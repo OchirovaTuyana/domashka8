23863e64ffda573fe0a5779d21a3d399a4f5f89e
Tuyana <tuyaochirova98@gmail.com>
2022-12-02 17:56:43 +0800

    zadacha 58

---
+++ zadacha58/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу,
+//которая будет находить произведение двух матриц.
+
+Console.Write("Введите количество строк 1 массива: ");
+int rowsA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 1 массива: ");
+int columnsA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк 2 массива: ");
+int rowsB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов 2 массива: ");
+int columnsB = Convert.ToInt32(Console.ReadLine());
+
+if (columnsA != rowsB)
+{
+    Console.WriteLine("Такие матрицы умножать нельзя!");
+    return;
+}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        result[i, j] = new Random().Next(min, max + 1);
+      }
+      
+    }
+    return result;
+}
+void printColor(string information, ConsoleColor color, bool newLine = false)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+    if (newLine)
+    {
+        Console.WriteLine();
+    }
+}
+void print2dArray(int[,] array, string Name = "")
+{
+    printColor(Name, ConsoleColor.DarkCyan, true);
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+          printColor(i + "\t", ConsoleColor.DarkYellow,(i >= array.GetLength(1) - 1));
+    }
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+int [,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+          for (int k = 0; k < arrayA.GetLength(1); k++)
+          {
+            arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+          }
+        }
+    }
+    return arrayC;
+}
+
+int[,] A = GetArray(rowsA, columnsA, 0, 10);
+int[,] B = GetArray(rowsB, columnsB, 0, 10);
+print2dArray(A, "Массив А");
+Console.WriteLine();
+print2dArray(B, "Массив B");
+Console.WriteLine();
+print2dArray(GetMultiplicationMatrix(A,B), "Массив C");
